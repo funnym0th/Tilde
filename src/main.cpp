92aba2f2e30bd13bd10afcf0332ccb55394add03
Camilo Catalán <mothbms@gmail.com>
2026-07-20 05:13:39 -0400

    feat: implement argument file handling to the entrypoint of the app

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
     MainWindow window;
+    if(app.arguments().size() > 1) {
+        window.openFile(app.arguments().at(1));
+    }
     window.show();
     return app.exec();
 }
