b8d4f7f1758f67175b1bec15b80cf10ac1733211
Camilo Catalán <mothbms@gmail.com>
2026-07-20 05:56:53 -0400

    chore: fix argument handling and enforce mimetypes

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <QApplication>
+#include <iostream>
 #include "mainwindow.hpp"
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
     MainWindow window;
-    if(app.arguments().size() > 1) {
-        window.openFile(app.arguments().at(1));
+
+
+
+    if(app.arguments().size() > 1 ) {
+        QString filePath = app.arguments().at(1);
+        if(filePath.endsWith(".md") || filePath.endsWith(".tex")) {
+            window.openFile(filePath);
+            window.show();
+            return app.exec();
+        }
+        else {
+            std::cout << "error: invalid file mimetype";
+        }
     }
-    window.show();
-    return app.exec();
+    else {
+        window.show();
+        return app.exec();
+    }
 }
