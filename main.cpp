bfc2aba497c2392bda6c4f2b170b7254690b60c1
Camilo Catalán <mothbms@gmail.com>
2026-07-20 01:50:47 -0400

    chore: change window declaration from heap to stack

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "MainWindow.hpp"
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
-    MainWindow* window = new MainWindow();
-    window -> show();
+    MainWindow window;
+    window.show();
     return app.exec();
 }
